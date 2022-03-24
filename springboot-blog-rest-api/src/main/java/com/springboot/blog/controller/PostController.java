1edc0e5b84f853088ddbd20352f88af0a4c10cc8
haekalzain <haekalzain77@gmail.com>
2022-03-24 19:02:29 +0700

    feat:rest api BE v1

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
@@ -0,0 +1,40 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    //create blog post REST API
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    //getAllPostRestApi
+    @GetMapping
+    public List<PostDto> getAllPost() {
+        return postService.getAllPost();
+    }
+
+    //getPostById
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+}
